813e1c715ecfaa985f6def511c4a1ee65ec78455
Sarvagya Singh <148546651+Sarvagya-10@users.noreply.github.com>
2024-02-21 16:16:44 +0000

    learnt about variables and comments

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 12305882;
+var name = "sarvagya";
+let email = "asdf@gmail.com";
+city="Jalandhar";
+/* askjldfjls
+lkjsakfjasl
+lksdfaljio
+city="Lucknow";*/
+// prefer not to use var, use let instead
+console.log(city);
+console.table([name,email,city,accountId]);
